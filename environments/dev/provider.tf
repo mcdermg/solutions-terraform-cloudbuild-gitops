fd74ce043f02926069d38bc7217e1d4ccd691c60
Gary Mc Dermott <mcdermg@tcd.ie>
2022-02-15 17:40:02 -0300

    tf version fix

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -3,7 +3,7 @@
 #####################################
 
 terraform {
-  required_version = "1.1.5"
+  required_version = "1.0.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
